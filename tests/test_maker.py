eaf3addc3c7499b6542ecbf6105032841157dca9
Josue K <josue.kpodo@gmail.com>
2020-06-16 16:03:58 -0400

    added test cases

--- tests/test_maker.py
+++ tests/test_maker.py
@@ -9,6 +9,10 @@ class NCToWTHConverterTestCase(unittest.TestCase):
         data = maker.NCToWTHConverter(inp)
         self.assertIsNone(data.check_dir(), "no nc files present")
 
+    def test_nc_files_present(self):
+        inp = 'D:/ddrive/agmerra_net_files'
+        data = maker.NCToWTHConverter(inp)
+        self.assertIsNotNone(data.check_dir(), "nc files present")
 
 if __name__ == '__main__':
     unittest.main()
