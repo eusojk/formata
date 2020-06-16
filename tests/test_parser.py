60ff928f667166973fa7d836c04d865792374f35
Josue K <josue.kpodo@gmail.com>
2020-06-16 00:54:33 -0400

    added some unit testing cases

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,18 @@
 import unittest
+from formata import parser
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class DataNetCDFTestCase(unittest.TestCase):
+
+    def test_ds_dir_unavailable(self):
+        wrong_dir = 'D:/ddrive/agmerra_net_filesa'
+        data = parser.DataNetCDF(wrong_dir)
+        self.assertIsNone(data.get_datasets(wrong_dir), "Result should be None when directory doesn't exist")
+
+    def test_no_nc_found(self):
+        wrong_dir = 'D:/ddrive/'
+        data = parser.DataNetCDF(wrong_dir)
+        self.assertIsNone(data.get_datasets(wrong_dir), "Result should be None when no .nc files found")
 
 
 if __name__ == '__main__':
