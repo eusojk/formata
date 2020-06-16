ed73e16426e13093825eb087f316a1624126aef8
Josue K <josue.kpodo@gmail.com>
2020-06-16 01:29:11 -0400

    renamed class name in parser.py

--- formata/parser.py
+++ formata/parser.py
@@ -1,20 +1,43 @@
+import glob
 import numpy as np
 import xarray as xr
+from pathlib import Path
 
 
-class DataNetCDF:
+class NCToWTHConverter:
     """
-
+    A Converter class to WTH files from gloabl NetCDF weather data
     """
 
     def __init__(self, nc_dir):
 
-        self.nc_dir = nc_dir
-        self.ds_all = xr.open_mfdataset(self.nc_dir, decode_times=False, combine='by_coords', engine='netcdf4',
-                                        parallel=True)
-        self.ds_dims = dict(self.ds_all.dims)
+        self.nc_dir = self.get_datasets(nc_dir)
+        if self.nc_dir is not None:
+            self.ds_all = self.merge_datasets()
+            self.ds_dims = dict(self.ds_all.dims)
+
+    def merge_datasets(self):
+        ds_all = xr.open_mfdataset(self.nc_dir, decode_times=False, combine='by_coords', engine='netcdf4',
+                                   parallel=True)
+        return ds_all
 
     def get_num_of_attribute(self, attr):
         if attr in self.ds_dims.keys():
             return self.ds_dims[attr]
         return
+
+    def get_global_dataset(self):
+        return self.ds_all
+
+    def get_datasets(self, nc_dir):
+
+        if not Path(nc_dir).exists():
+            print('No such directory found:', nc_dir)
+            return
+
+        nc_all = nc_dir + "/*.nc*"
+        if len(glob.glob(nc_all)) == 0:
+            print('No NetCDF files found in:', nc_dir)
+            return
+
+        return nc_all
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,12 +6,12 @@ class DataNetCDFTestCase(unittest.TestCase):
 
     def test_ds_dir_unavailable(self):
         wrong_dir = 'D:/ddrive/agmerra_net_filesa'
-        data = parser.DataNetCDF(wrong_dir)
+        data = parser.NCToWTHConverter(wrong_dir)
         self.assertIsNone(data.get_datasets(wrong_dir), "Result should be None when directory doesn't exist")
 
     def test_no_nc_found(self):
         wrong_dir = 'D:/ddrive/'
-        data = parser.DataNetCDF(wrong_dir)
+        data = parser.NCToWTHConverter(wrong_dir)
         self.assertIsNone(data.get_datasets(wrong_dir), "Result should be None when no .nc files found")
 
 
